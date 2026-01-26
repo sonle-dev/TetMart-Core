5d5df9dbe431181109c9cd036ddbfd5a7da544fc
Le Hong Son <lehongson270920050@gmail.com>
2026-01-26 07:07:36 +0700

    feat: hoan thien logic Authentication, xu ly don hang va API mua ngay

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from products.models import Product 
+from .models import Order
 
-# Create your views here.
+# xu li mua ngay
+@login_required(login_url='login')
+def buy_now_view(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    
+# Tao don moi
+    Order.objects.create(
+        user=request.user,
+        total_price=product.price, 
+        status='pending'
+    )
+    
+    messages.success(request, f"Đã đặt mua thành công: {product.name}!")
+    return redirect('home')
--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         
-        fields = ('username', 'email', 'phone', 'address', 'avatar')
+        fields = ('username', 'email')
