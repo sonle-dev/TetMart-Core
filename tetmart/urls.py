23483a5e0fb42d08e495fbef2f618cb53ec7dfe4
Le Hong Son <lehongson270920050@gmail.com>
2026-03-09 03:02:00 +0700

    fix: gỡ xung đột merge và khôi phục cấu hình URL dự án

--- tetmart/urls.py
+++ tetmart/urls.py
@@ -3,37 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-<<<<<<< Updated upstream
-
-
-from products import views as product_views 
-from products.views import home, product_detail
-from users.views import report_view 
-
-from orders.views import buy_now_view 
-
-urlpatterns = [
-    #  Trang Django
-    path('admin/', admin.site.urls),
-
-    #  Trang chu va san pham
-    path('', home, name='home'),
-    path('product/<int:pk>/', product_detail, name='product_detail'),
-
-    #  xac thuc quyen
-    path('auth/', include('users.urls')),
-    path('dashboard/', product_views.dashboard_view, name='dashboard'),
-    
-    # cap nhat trang thai
-    path('dashboard/order/<int:pk>/', product_views.order_detail_view, name='order_detail'),
-    
-    # danh sach don hang
-    path('dashboard/orders/', product_views.dashboard_orders_view, name='dashboard_orders'),
-    
-    # danh sach san pham
-    path('dashboard/products/', product_views.dashboard_products_view, name='dashboard_products'),
-    path('dashboard/report/', product_views.report_view, name='report'),
-=======
 from orders.views import buy_now_view
 
 urlpatterns = [
@@ -46,7 +15,6 @@ urlpatterns = [
     path('auth/', include('users.urls')),
 
     # Buy now
->>>>>>> Stashed changes
     path('buy-now/<int:product_id>/', buy_now_view, name='buy_now'),
 
     # Cart
